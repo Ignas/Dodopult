7da51cb1c4242ae4eb74a76fbed8dbd364f4f57c
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-01-29 10:24:05 +0200

    Made me a catapult.

--- dodo.py
+++ dodo.py
@@ -6,20 +6,34 @@ window = pyglet.window.Window(width=1024, height=786)
 font = dict(font_name='Andale Mono',
             font_size=20)
 
-me = pyglet.text.Label('@', x=400, y=300, **font)
-me.height = 20
-me.width = 16
 
+me_text = pyglet.text.document.UnformattedDocument('\n\nu--@')
+me_text.set_style(0, len(me_text.text), {
+            'font_name': 'Andale Mono',
+            'font_size': 20,
+            'color': (255, 255, 255, 255)
+        })
+
+me = pyglet.text.layout.TextLayout(me_text, 200, 200, multiline=True)
+me.x = 500
+me.y = 400
 
 window.push_handlers(pyglet.window.event.WindowEventLogger())
+
 
 @window.event
 def on_text_motion(motion):
     if motion == key.LEFT:
-        me.x -= me.width
+        me.x -= 16
     elif motion == key.RIGHT:
-        me.x += me.width
+        me.x += 16
 
+@window.event
+def on_key_press(symbol, modifiers):
+     if symbol == key.SPACE:
+        me_text.text = '\n'.join(['   c',
+                                  '   |',
+                                  '   @'])
 
 fps_display = pyglet.clock.ClockDisplay()
 
